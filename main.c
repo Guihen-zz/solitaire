16924ec69bb45c42bb5f925bb8d9d4ce349a92e3
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-02-24 16:59:34 -0300

    Template for Solitaire.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void) 
+{
+  // This panel contains 59 chars.
+  // 24 spaces between the title and the form.
+  printf ("+---------------------------------------------------------+\n");
+  printf ("|                        Solitaire                        |\n");
+  printf ("+---------------------------------------------------------+\n");
+  printf ("|  (%2d) [ %2d ]             [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ] |\n", 23, 0, 1, 2, 3, 4);
+  printf ("|                                                         |\n");
+  printf ("|                                                         |\n");
+  printf ("|  [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ] |\n", 1, 2, 3, 4, 5, 6, 7);
+  printf ("|_________________________________________________________|\n");
+  printf (" Move description: \n");
+  
+  return 0;
+}
