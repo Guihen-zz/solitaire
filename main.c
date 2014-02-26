b06174e7a3591ac9d6615568050dc2967d6e8853
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-02-26 11:54:47 -0300

    Functions to manage deck
    One to get deck from input and other to print a deck.

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "CARD.h"
+
+#define DECKSIZE 48
+
+Card *get_deck ()
+{
+  int deck_size = DECKSIZE;
+  char *line = malloc (sizeof(5));
+  Card card;
+  Card *deck = malloc(sizeof(Card *) * DECKSIZE);
+  
+  while (deck_size) {
+    card = malloc(sizeof(Card));
+    fgets(line, 5, stdin); /* <Rank><space><Suit><enter>\0 */
+    card->rank = line[0];
+    card->suit = line[2];
+    deck[--deck_size] = card;
+  }
+  return deck;
+}
+
+void show_deck (Card *deck)
+{
+  int i;
+  for (i = 0; i < DECKSIZE; i++)
+    printf ("%c %c\n", deck[i]->rank, deck[i]->suit);
+}
 
 int main (void) 
 {
+  Card *deck = get_deck();
+  show_deck (deck); 
+  
   /* This panel contains 59 chars. */
   /* 24 spaces between the title and the form. */
   printf ("+---------------------------------------------------------+\n");
