bfd5dd28e232daaf255b5f630fc2bd8ceab7de61
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-02-28 22:21:47 -0300

    Field face_up to Card type

--- CARD.h
+++ CARD.h
@@ -8,16 +8,19 @@
  * email: guihen@linux.ime.usp.br
  *
  * This file creates a representation of a card of the deck as a struct with 2
- * fields: Suit, that is an abstraction of the Suit (see SUIT.h) and a char that 
- * stores the rank of the deck (A, 2, 3, ..., J, Q, K).
+ * fields: Suit, that is an abstraction of the Suit (see SUIT.h), a char that 
+ * stores the rank of the deck (A, 2, 3, ..., J, Q, K) and a boolean to store if
+ * it is face up or not.
  ******************************************************************************/
 
+#include <stdbool.h>
 #include "SUIT.h"
 
 struct card
 {
   Suit suit;
   char rank;
+  bool face_up;
 };
 typedef struct card *Card;
 
