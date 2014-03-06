53c9174d52a7ca1dd49329ca06c475403902080f
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-03-06 11:38:47 -0300

    Improved print function

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ Card *get_deck ()
   int deck_size = DECKSIZE;
   char *line = malloc (5);
   Card card;
-  Card *deck = malloc(sizeof(Card *) * DECKSIZE);
-  
+  Card *deck = malloc(sizeof(Card) * DECKSIZE);
+
   while (deck_size) {
-    card = malloc(sizeof(Card));
+    card = malloc(sizeof(*card));
     fgets(line, 5, stdin); /* <Rank><space><Suit><enter>\0 */
     card->rank = line[0];
     card->suit = line[2];
@@ -28,15 +28,14 @@ void show_deck (Card *deck)
 {
   int i;
   for (i = 0; i < DECKSIZE; i++)
-    if (deck[i]->face_up)
-      printf ("%c %c\n", deck[i]->rank, deck[i]->suit);
+    printf ("%c %c\n", deck[i]->rank, deck[i]->suit);
 }
 
 CardStack *init_solitaire_template()
 {
   int i;
   CardStack *template = malloc (sizeof (CardStack) * 7);
-  
+
   for (i = 0; i < 7; i++)
   {
     template[i] = new_stack();
@@ -57,27 +56,47 @@ CardStack* prepare_solitaire(Card *deck)
   return template;
 }
 
-void print(CardStack *solitaire)
+void print (CardStack *solitaire)
 {
-  int i, j, deck_index = 0;
+  int i, j;
+  Node node[7];
+  Card card;
+  bool exit_condition;
+
   for (i = 0; i < 7; i++)
+    node[i] = get_last_node (solitaire[i]);
+
+  i = 0, exit_condition = false;
+  while (!exit_condition)
   {
-    printf ("| ");
-    
-    for (j = 0; j < 7; j++)
+    if (i == 0) printf ("| ");
+
+    if (node[i] != NULL)
     {
-      if (j >= i)
-      {
-        printf("  [XX]  ");
-      }
-      else printf("        ");
+      card = get_card (node[i]);
+      printf("  [%c%c]  ", card->suit, card->rank);
+
+      node[i] = previous_node (node[i]);
+    }
+    else printf("        ");
+
+    if (++i == 7)
+    {
+      printf ("|\n");
+      i = 0;
     }
 
-    printf ("|\n");
+    /* Exit Condition. */
+    for (j = 0; j < 7; j++)
+    {
+      /* INVARIANT RELATION: all the nodes with index < j are null nodes. */
+      if (node[j] != NULL) break;
+    }
+    if (j == 7) exit_condition = true;
   }
 }
 
-int main (void) 
+int main (void)
 {
   Card *deck = get_deck();
   CardStack *solitaire = prepare_solitaire (deck);
@@ -90,11 +109,11 @@ int main (void)
   printf ("|   (%2d) [ %2d ]           [ %2d ]  [ %2d ]  [ %2d ]  [ %2d ]  |\n", 23, 0, 1, 2, 3, 4);
   printf ("|                                                         |\n");
   printf ("|                                                         |\n");
-  
+
   print (solitaire);
 
   printf ("|_________________________________________________________|\n");
   printf (" Move description: \n");
-  
+
   return 0;
 }
